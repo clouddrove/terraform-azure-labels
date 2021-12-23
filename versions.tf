63ad54808b7e6055e87ffbc37b7c6ee1c82913e2
deepakvermacloud <deepak.verma@clouddrove.com>
2021-12-23 19:37:52 +0530

    terraform version updated & .github added & _main.tf changed to example.tf

--- versions.tf
+++ versions.tf
@@ -1,13 +1,13 @@
 # Terraform version
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 1.0.0"
 }
 
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.46.0"
+      version = "=2.90.0"
     }
   }
 }
